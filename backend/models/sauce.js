c138ca08adf7d1f6b8b365f042a86e494748b33d
jinyan yang <jinyan.yang627@gmail.com>
2020-11-24 14:36:37 +0100

    schama sauce

---
+++ backend/models/sauce.js
@@ -0,0 +1,19 @@
+//importer le package mongoose dans ce fichier
+const mongoose = require('mongoose');
+
+//création le modèle du schéma de données qui contient les champs souhaités 
+const sauceSchema = mongoose.Schema({
+  userId: { type: String, required: true },
+  name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  description: { type: String, required: true },
+  mainPepper: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  heat: { type: Number, required: true },
+  likes: { type: Number, default: 0 },
+  dislikes: { type: Number, default: 0 },
+  usersLiked: { type: [String] },
+  usersDisliked: { type: [String] },
+});
+//exporter ce schéma en tant que modèle Mongoose appelé « Sauce »
+module.exports = mongoose.model('Sauce', sauceSchema);
