8791097a0abbf180a8762ed19c337d90e48810f2
jinyan yang <jinyan.yang627@gmail.com>
2020-12-02 10:39:03 +0100

    middleware auth

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -4,15 +4,16 @@ const express = require('express');
 const router = express.Router();
 //importer la logique métier de nos routes
 const sauceCtrl = require('../controllers/sauce');
-
+//importer middleware auth
+const auth = require('../middleware/auth');
 
 
 // Routes
-router.get('/',sauceCtrl.getAllSauces);
-router.get('/:id',sauceCtrl.getOneSauce);
-router.post('/',sauceCtrl.createSauce);
-router.put('/:id',sauceCtrl.modifySauce);
-router.delete('/:id', sauceCtrl.deleteSauce);
+router.get('/', auth, sauceCtrl.getAllSauces);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.post('/', auth, sauceCtrl.createSauce);
+router.put('/:id', auth, sauceCtrl.modifySauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
 
 
 module.exports = router;
